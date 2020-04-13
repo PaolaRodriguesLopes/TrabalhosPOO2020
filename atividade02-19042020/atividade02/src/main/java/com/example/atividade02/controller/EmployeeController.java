b9b56902e697039ddb32e16a88cac1e17f9add67
PaolaRodriguesLopes <56982578+PaolaRodriguesLopes@users.noreply.github.com>
2020-04-12 21:42:26 -0300

    Controller

---
+++ atividade02-19042020/atividade02/src/main/java/com/example/atividade02/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.example.atividade02.controller;
+
+import com.example.atividade02.service.EmployeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+
+    //Devolve todos os funcionarios para o template
+    @GetMapping("/funcionarios")
+    public ModelAndView getEmployee ()
+    {
+        ModelAndView mv = new ModelAndView("Funcionarios");
+
+        mv.addObject("alunos", service.getEmployee());
+
+        return mv;
+    }
+}
